852e6ab3585f2b3c93f6d412b8aacc412e053ffa
Wooseop Kim <wooseop@protonmail.com>
2024-04-07 23:59:08 +0900

    Remove `ls` call

--- install.sh
+++ install.sh
@@ -23,7 +23,6 @@ sudo npm i -g appium@next && \
 `# https://appium.io/docs/en/2.0/quickstart/uiauto2-driver/`
 brew install gradle `# https://gist.github.com/patrickhammond/4ddbe49a67e5eb1b9c03` `# https://formulae.brew.sh/formula/gradle` && \
 brew install --cask android-platform-tools `# https://formulae.brew.sh/cask/android-platform-tools` && \
-ls $HOME/Library/Android/sdk/** && \
 brew tap homebrew/cask-versions && brew install --cask temurin17 `# https://adoptium.net/installation/` && \
 echo '
 export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
