42b2e13a66876c2672cf99867a283357bdff6be0
Wooseop Kim <wooseop@protonmail.com>
2024-04-07 23:55:39 +0900

    Add `cmdline-tools/latest` to `PATH`

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ brew tap homebrew/cask-versions && brew install --cask temurin17 `# https://adop
 echo '
 export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
+export PATH=$PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest
 export PATH=$PATH:$ANDROID_SDK_ROOT/tools
 export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
 export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
