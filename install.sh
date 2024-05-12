727a5635bb522e40706f39bd91eadc46ef7643a8
Wooseop Kim <wooseop@protonmail.com>
2024-05-12 21:56:48 +0900

    Manipulate `GITHUB_ENV` when on GitHub Actions

--- install.sh
+++ install.sh
@@ -15,6 +15,13 @@ add_path () {
     echo "export PATH=\$PATH:$1" >> ~/.bashrc
     source ~/.bashrc
 }
+set_env () {
+    if [ $GITHUB_ACTIONS ]; then
+        echo "$1=$2" >> "$GITHUB_ENV"
+    fi
+    echo "export $1=$2" >> ~/.bashrc
+    source ~/.bashrc
+}
 
 # https://brew.sh/#install
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
@@ -26,7 +33,7 @@ sudo npm i -g appium@next && \
 `# https://appium.io/docs/en/2.0/quickstart/uiauto2-driver/`
 brew install --cask android-platform-tools `# https://formulae.brew.sh/cask/android-platform-tools` && \
 brew install --cask temurin@17 `# https://formulae.brew.sh/cask/temurin@17#default` && \
-export JAVA_HOME=`/usr/libexec/java_home -v 17` && \
+set_env JAVA_HOME `/usr/libexec/java_home -v 17` && \
 ANDROID_SDK_ROOT=$HOME/Library/Android/sdk && \
 add_path "$ANDROID_SDK_ROOT/emulator" && \
 add_path "$ANDROID_SDK_ROOT/cmdline-tools/latest/bin" && \
