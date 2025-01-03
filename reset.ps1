61ed65d87942e28c943359d053c0f84cb23b1f3e
Wooseop Kim <wooseop@protonmail.com>
2025-01-03 21:40:59 +0900

    Add scripts to clean environment

---
+++ reset.ps1
@@ -0,0 +1,63 @@
+# https://github.com/PowerShell/PowerShell/issues/3415#issuecomment-1354457563
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
+if ($env:CI -ne 'true') {
+    exit 1
+}
+
+function Remove-Command {
+    param (
+        $name
+    )
+    where.exe "$name" | ForEach-Object { Remove-Item $_ }
+}
+
+function Unset-Environment {
+    param (
+        $pattern
+    )
+    Get-ChildItem env:*$pattern* -Name | ForEach-Object { [Environment]::SetEnvironmentVariable($_, $null, [EnvironmentVariableTarget]::Machine) }
+}
+
+function Ensure-Not-Callable {
+    param (
+        $name
+    )
+    where.exe "$name" *> $null
+    -Not $?
+}
+
+function Ensure-Environment-Empty {
+    param (
+        $pattern
+    )
+    -Not Test-Path -Path :env:*$pattern*
+}
+
+@"
+    sdkmanager
+    avdmanager
+    java
+    gradle
+    node
+    npm
+    python
+    appium
+    choco
+"@.Split([Environment]::NewLine) | ForEach-Object {
+    $name = $_.Trim()
+    Remove-Command $name
+    Ensure-Not-Callable $name
+}
+
+@"
+    JAVA
+    ANDROID
+    Chocolately
+"@.Split([Environment]::NewLine) | ForEach-Object {
+    $pattern = $_.Trim()
+    Unset-Environment $pattern
+    Ensure-Environment-Empty $pattern
+}
---
+++ reset.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash -eu -o pipefail
+
+if [ "${CI:-}" != 'true' ]; then
+    exit 1
+fi
+
+remove_command () {
+    which -a $1 | xargs rm
+}
+
+unset_environment () {
+    unset `awk 'BEGIN{for(v in ENVIRON) print v}' | grep $1`
+}
+
+ensure_not_callable () {
+    return $(which $1 | wc -l)
+}
+
+ensure_environment_empty () {
+    return $(awk 'BEGIN{for(v in ENVIRON) print v}' | grep $1 | wc -l)
+}
+
+while read name; do
+    remove_command $name
+    ensure_not_callable $name
+done <<-EOF
+    sdkmanager
+    avdmanager
+    java
+    gradle
+    node
+    npm
+    python
+    appium
+    choco
+EOF
+
+while read pattern; do
+    unset_environment $pattern
+    ensure_environment_empty $pattern
+done <<-EOF
+    JAVA
+    ANDROID
+EOF
