26368678481be44a3bc21bc71646a1446cc63ab0
Wooseop Kim <wooseop@protonmail.com>
2025-01-04 00:21:19 +0900

    Group outputs

--- verify.ps1
+++ verify.ps1
@@ -3,20 +3,32 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
+echo '::group::node'
 node --version
 where.exe node
+echo '::endgroup::'
 
+echo '::group::appium'
 appium --version
 where.exe appium
+echo '::endgroup::'
 
+echo '::group::java'
 java -version
 where.exe java
+echo '::endgroup::'
 
+echo '::group::sdkmanager'
 sdkmanager --list
 where.exe sdkmanager
+echo '::endgroup::'
 
+echo '::group::avdmanager'
 avdmanager list device
 where.exe avdmanager
+echo '::endgroup::'
 
+echo '::group::python'
 python --version
 where.exe python
+echo '::endgroup::'
--- verify.sh
+++ verify.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash -eu -o pipefail
 
+echo '::group::node'
 node --version
 whereis node
+echo '::endgroup::'
 
+echo '::group::appium'
 appium --version
 whereis appium
+echo '::endgroup::'
 
+echo '::group::java'
 java -version
 whereis java
+echo '::endgroup::'
 
+echo '::group::sdkmanager'
 sdkmanager --list
 whereis sdkmanager
+echo '::endgroup::'
 
+echo '::group::avdmanager'
 avdmanager list device
 whereis avdmanager
+echo '::endgroup::'
 
+echo '::group::python'
 python --version
 whereis python
+echo '::endgroup::'
